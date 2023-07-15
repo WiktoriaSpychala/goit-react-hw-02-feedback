539913f22def9f4e55d1610cdb18021c966201a9
Wiktoria Spychała <wiktoria_spychala@o2.pl>
2023-07-15 20:20:40 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const percentage = Math.round((this.state.good / (this.state.good + this.state.neutral + this.state.bad)) * 100);
+    return percentage;
+  };
+  handleClick = key => {
+    this.setState(state => ({
+      [key]: state[key] + 1,
+    }));
+  };
+  render() {
+    const options = Object.keys(this.state);
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={options}
+            handleClick={this.handleClick} />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
